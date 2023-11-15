24cb77140117d0e3af36d5e56b4af632483b138a
Eric Lin <dxslyofficial@gmail.com>
2023-11-14 19:09:27 -0500

    yes

---
+++ main.js
@@ -0,0 +1,8 @@
+// import kaboom lib
+import kaboom from "https://unpkg.com/kaboom@3000.0.1/dist/kaboom.mjs";
+
+// initialize kaboom context
+kaboom();
+
+// add a piece of text at position (120, 80)
+add([text("hello"), pos(120, 80)]);
